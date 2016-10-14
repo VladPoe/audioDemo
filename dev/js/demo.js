bcd039fcb56d466b135a82380ca58648cd4aedaa
pvs <pvs@htc-cs.ru>
2016-10-14 11:56:37 +0400

    commit

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,28 +1,47 @@
 
 var context = new AudioContext(),
-    oscillator = null;
+    mousedown = false,
+    oscillator;
+var gainNode = context.createGain();
 
 document.body.addEventListener('mousedown', function (e) {
-    console.log(e.clientY);
+    mousedown = true;
     oscillator = context.createOscillator();
-    //oscillator.frequency.value = calculateFrequency(e.clientY);
-    oscillator.frequency.setTargetAtTime(calculateFrequency(e.clientY), context.currentTime, 0.2);
-    oscillator.connect(context.destination);
+    oscillator.frequency.setTargetAtTime(calculateFrequency(e.clientX), context.currentTime, 0.05);
+    oscillator.connect(gainNode);
+    gainNode.connect(context.destination);
     oscillator.start(context.currentTime);
 });
 
+
 document.body.addEventListener('mouseup', function () {
-    if (oscillator) {
-        oscillator.stop(context.currentTime);
-        oscillator.disconnect();
+    mousedown = false;
+    oscillator.stop(context.currentTime);
+    oscillator.disconnect();
+});
+
+
+document.body.addEventListener('mousemove', function (e) {
+    if (mousedown) {
+        oscillator.frequency.setTargetAtTime(calculateFrequency(e.clientX), context.currentTime, 0.05);
+        gainNode.gain.setTargetAtTime(calculateGain(e.clientY), context.currentTime, 0.01);
     }
 });
 
 
-var calculateFrequency = function (mouseYPosition) {
+var calculateFrequency = function (mouseXPosition) {
     var minFrequency = 20;
     var maxFrequency = 2000;
+    var windowWidth = window.innerWidth;
+
+    return ((mouseXPosition / windowWidth) * maxFrequency) + minFrequency;
+};
+
+var calculateGain = function (mouseYPosition) {
+    var minVolume = 0;
+    var maxVolume = 1;
     var windowHeight = window.innerHeight;
 
-    return ((mouseYPosition / windowHeight) * maxFrequency) + minFrequency;
+    return 1 - ((mouseYPosition / window.innerHeight) * maxVolume) + minVolume;
+
 };
